d18b16b3f0cac97bb612d8bd370b90ed417856de
Mark Seaborn <mrs@mythic-beasts.com>
2008-12-28 18:51:41 +0000

    Enable debugging

--- myconfig.sh
+++ myconfig.sh
@@ -6,6 +6,6 @@ mkdir -p build4
 cd build4
 ../configure --prefix=/usr --host=i486-linux-gnu \
     CC="`pwd`/../../tools_bin/linux/sdk/nacl-sdk/bin/nacl-gcc" \
-    CFLAGS="-march=i486 -pipe -fstrict-aliasing -O2" \
+    CFLAGS="-march=i486 -pipe -fstrict-aliasing -O2 -g" \
     --with-headers=`pwd`/../kernel-headers \
     --disable-shared
