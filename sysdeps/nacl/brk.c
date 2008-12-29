2e8c2106e4b1bea84b1ab2c656bc29ce54dc1366
Mark Seaborn <mrs@mythic-beasts.com>
2008-12-29 00:31:32 +0000

    Don't depend on libnacl

--- sysdeps/nacl/brk.c
+++ sysdeps/nacl/brk.c
@@ -6,13 +6,15 @@
 void *__curbrk = 0;
 weak_alias (__curbrk, ___brk_addr)
 
-int __NaClSysBrk(int);
-int __nacl_write(int desc, void const *buf, size_t count);
-// __attribute__((weak));
+__asm__(".global __gnacl_write; __gnacl_write = 0x11a0 ");
+__asm__(".global __NaClSysBrk; __NaClSysBrk = 0x1280");
+
+void *__NaClSysBrk(void *addr);
+int __gnacl_write(int desc, void const *buf, size_t count);
 
 int __brk (void *addr)
 {
-  __nacl_write(1, "hi there!\n", 10);
+  __gnacl_write(1, "hi there!\n", 10);
   __curbrk = __NaClSysBrk(addr);
   return 0;
   //__set_errno (ENOMEM);
