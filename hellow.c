6e78885f993e79436db527df28529d6a1b52242d
Mark Seaborn <mrs@mythic-beasts.com>
2008-12-30 12:15:59 +0000

    Add example hellow.c program and script for building it

---
+++ hellow.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main()
+{
+  write(1, "hello\n", 6);
+  //printf("Hello world %i\n", 42);
+  return 0;
+}
---
+++ make-example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BUILDDIR=build4
+../tools_bin/linux/sdk/nacl-sdk/bin/nacl-gcc \
+    -B$BUILDDIR/csu/ -L$BUILDDIR -static -Wl,-T,elf_i386.x \
+    -g hellow.c -o hellow \
+    -Wl,-print-map > hellow.map
