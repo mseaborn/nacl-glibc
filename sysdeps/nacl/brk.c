4e6d05756f84cb5e71467d5c159faf2f41e17a6b
Mark Seaborn <mrs@mythic-beasts.com>
2008-12-30 12:12:43 +0000

    Remove debugging code from brk()

--- sysdeps/nacl/brk.c
+++ sysdeps/nacl/brk.c
@@ -6,18 +6,13 @@
 void *__curbrk = 0;
 weak_alias (__curbrk, ___brk_addr)
 
-__asm__(".global __gnacl_write; __gnacl_write = 0x11a0 ");
 __asm__(".global __NaClSysBrk; __NaClSysBrk = 0x1280");
 
 void *__NaClSysBrk(void *addr);
-int __gnacl_write(int desc, void const *buf, size_t count);
 
 int __brk (void *addr)
 {
-  __gnacl_write(1, "hi there!\n", 10);
   __curbrk = __NaClSysBrk(addr);
   return 0;
-  //__set_errno (ENOMEM);
-  //return -1;
 }
 weak_alias (__brk, brk)
