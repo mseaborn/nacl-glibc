7abc398b626416a29d9178a7356871ac9f010db8
Mark Seaborn <mrs@mythic-beasts.com>
2009-01-10 16:14:56 +0000

    Automate creation of kernel-headers directory in myconfig.sh

--- myconfig.sh
+++ myconfig.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# Since we are taking Linux glibc as a starting point we need the
+# Linux kernel headers.  In the long term we will need Linux's errno
+# numbers, but probably nothing more.
+rm -rf kernel-headers
+mkdir kernel-headers
+cp -a /usr/include/asm /usr/include/asm-generic /usr/include/linux \
+    kernel-headers/
+
 BUILDDIR=build
 mkdir -p $BUILDDIR
 cd $BUILDDIR
